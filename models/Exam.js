95368eeb7a5041992eeb3562c94837eb4621deb5
milan-codes <milan.herke.business@gmail.com>
2020-07-04 13:02:10 +0200

    Added data models

---
+++ models/Exam.js
@@ -0,0 +1,48 @@
+/**
+ * @class     Exam
+ * @classdesc This class defines the structure of an Exam object.
+ */
+class Exam {
+  /**
+   * @param {string}  name - Name of the exam (mandatory)
+   * @param {string}  description - A short description, notes (optional)
+   * @param {string}  subjectId - ID of a [Subject] object (mandatory)
+   * @param {Date}    dueDate - The date of the exam (mandatory)
+   * @param {Date}    reminder - Date of reminder (optional)
+   * @param {string}  id - Unique ID, automatically set
+   */
+  constructor(name, description, subjectId, dueDate, reminder = null, id = "") {
+    /**
+     * @name  Exam#name
+     * @type  {string}
+     */
+    this.name = name;
+    /**
+     * @name    Exam#description
+     * @type    {string}
+     */
+    this.description = description;
+    /**
+     * @name    Exam#subjectId
+     * @type    {string}
+     */
+    this.subjectId = subjectId;
+    /**
+     * @name    Exam#dueDate
+     * @type    {Date}
+     */
+    this.dueDate = dueDate;
+    /**
+     * @name    Exam#reminder
+     * @type    {Date}
+     * @default null
+     */
+    this.reminder = reminder;
+    /**
+     * @name    Exam#id
+     * @type    {string}
+     * @default ""
+     */
+    this.id = id;
+  }
+}
---
+++ models/Task.js
@@ -0,0 +1,54 @@
+/**
+ * @class     Task
+ * @classdesc This class defines the structure of a Task object.
+ */
+class Task {
+  /**
+   * @param {string}  name - Name of the task (mandatory)
+   * @param {string}  description - Description of the task (optional)
+   * @param {number}  type - Type of the task, either assignment or revision (mandatory)
+   * @param {string}  subjectId - ID of the task's subject, a [Subject] ID (mandatory)
+   * @param {Date}    dueDate - Due date of the task (mandatory)
+   * @param {Date}    reminder - Date of reminder (optional)
+   * @param {string}  id - Unique ID, automatically set
+   */
+  constructor(name, description, type, subjectId, dueDate, reminder = null, id = "") {
+    /**
+     * @name    Task#name
+     * @type    {string}
+     */
+    this.name = name;
+    /**
+     * @name    Task#description
+     * @type    {string}
+     */
+    this.description = description;
+    /**
+     * @name    Task#type
+     * @type    {number}
+     */
+    this.type = type;
+    /**
+     * @name    Task#subjectId
+     * @type    {string}
+     */
+    this.subjectId = subjectId;
+    /**
+     * @name    Task#dueDate
+     * @type    {Date}
+     */
+    this.dueDate = dueDate;
+    /**
+     * @name    Task#reminder
+     * @type    {Date}
+     * @default null
+     */
+    this.reminder = reminder;
+    /**
+     * @name    Task#id
+     * @type    {string}
+     * @default ""
+     */
+    this.id = id;
+  }
+}
